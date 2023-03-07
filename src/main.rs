4401f5fdf7225accc10afadae2eb089b6a85914e
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2023-03-08 00:15:38 +0900

    add: some log crates

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::env;
-
 use actix_web::{
     HttpServer,
     App
 };
+use actix_web::middleware::Logger;
 use dotenv::dotenv;
+use env_logger::Env;
 
 mod router;
 
@@ -12,10 +12,12 @@ mod router;
 async fn main() -> std::io::Result<()> {
     /* Init */
     std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init_from_env(Env::default().default_filter_or("debug"));
     dotenv().ok();
     /* Start HTTP Server */
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .service(router::index)
     })
     .bind(("127.0.0.1", 8080))?
