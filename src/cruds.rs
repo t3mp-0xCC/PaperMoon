87b764ebe71596e0d87dac50fcc179578d299378
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2023-03-12 00:28:08 +0900

    add: get_post_from_id

--- src/cruds.rs
+++ src/cruds.rs
@@ -1,8 +1,13 @@
-use anyhow::Context;
-use diesel::{insert_into, RunQueryDsl};
+use anyhow::{anyhow, Context};
+use diesel::{
+    insert_into,
+    ExpressionMethods,
+    RunQueryDsl,
+    QueryDsl
+};
 
 use crate::db::establish_connection;
-use crate::models::NewPost;
+use crate::models::{NewPost, Post};
 use crate::schema::posts;
 
 pub fn insert_new_post (
@@ -16,4 +21,30 @@ pub fn insert_new_post (
         .execute(conn)
         .with_context(|| "Failed to insert new_post")?;
     Ok(())
+}
+
+pub fn get_post_from_id(cid: &String) -> anyhow::Result<Post> {
+    use crate::schema::posts::dsl::*;
+    let conn = &mut establish_connection()?;
+    match posts.filter(content_id.eq(cid)).first::<Post>(conn) {
+        Ok(p) => Ok(p),
+        Err(_) => return Err(anyhow!("Failed to filter posts")),
+    }
+}
+
+#[cfg(test)]
+mod cruds_tests {
+    use super::*;
+
+    #[test]
+    fn search_from_id() {
+        let content_id = String::from("search_from_id");
+        let title = String::from("Asylum Piece");
+        let content_html = String::from("Knock Knock");
+        insert_new_post(&content_id, &title, &content_html)
+            .expect("Failed to insert test post");
+        let test_post = get_post_from_id(&content_id)
+            .expect("Failed to search from id");
+        assert_eq!(test_post.title, title)
+    }
 }
