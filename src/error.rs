fb0932d96271bf43209f56508d9e0344e79ae58c
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2023-07-05 13:39:51 +0900

    improve: error page rendering

--- src/error.rs
+++ src/error.rs
@@ -1,20 +1,59 @@
 use actix_web::{
     dev::ServiceResponse,
-    http::header::ContentType,
+    http::header::{ContentType, HeaderValue},
     middleware::ErrorHandlerResponse,
     HttpResponseBuilder,
     Result,
 };
 
-/* HTTP Error used in PaperMoon
- - NotFound
- - InternalServerError
- */
+
 
 pub fn render_error_page<B>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
     let status = res.status();
-    let request = res.into_parts().0;
-    let html = "<h1> Error 0w0 </h1>";
+    let (request, response) = res.into_parts();
+    let def_msg = HeaderValue::from_static("");
+    let msg = response.headers().get("X-Err-Msg")
+        .unwrap_or(&def_msg)
+        .to_str()
+        .unwrap();
+    let html = format!("
+        <!DOCTYPE html>
+        <head>
+            <title> Error ! - PaperMoon</title>
+            <link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"/misc/favicon.ico\"/>
+            <link rel=\"stylesheet\" href=\"/static/style.css\"/>
+            <style>
+                body {{
+                    background-color: #282828;
+                    margin: 0;
+                    padding: 0;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    height: 100vh;
+                    text-align: center;
+                }}
+
+                .status-code {{
+                    font-size: 5rem;
+                    font-weight: bold;
+                    color: #CC241D;
+                    margin-bottom: 1rem;
+                }}
+
+                .error-message {{
+                    font-size: 2rem;
+                    color: #869D6A;
+                }}
+            </style>
+        </head>
+        <body>
+            <div class=\"content\">
+                <div class=\"status-code\">{}</div>
+                <class=\"error-message\">{}</div>
+            </div>
+        </body>
+    ", status, msg);
 
     let new_response = HttpResponseBuilder::new(status)
     .insert_header(ContentType::html())
